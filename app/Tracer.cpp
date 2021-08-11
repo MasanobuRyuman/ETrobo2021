3e17342ef4a4c5cbd8da57f93302c1d9fcf33b8c
ryumanmasanobu <22011376mr@tama.ac.jp>
2021-08-11 17:07:42 +0900

    黄色輪っかを超えた検知

--- app/Tracer.cpp
+++ app/Tracer.cpp
@@ -1,5 +1,6 @@
 #include "Tracer.h" 
 #include <Motor.h> 
+#include "Clock.h"
 using namespace ev3api;  
 
 Tracer::Tracer():
@@ -16,14 +17,51 @@ void Tracer::terminate() {
   rightWheel.stop();
 }
 
+//p制御
 float Tracer::calc_porp_value(){
   const int bias = 0;
-  int diff = colorSensor.getBrightness() - target;
-  return (maxkp * diff + bias);
+  int diff;
+  if (line_status == true){
+    diff = colorSensor.getBrightness() - blue_target;
+  }else{
+    diff = colorSensor.getBrightness() - target;
+  }
+  
+  return (kp * diff + bias);
 }
 
+//I制御の簡単な実装。
+float Tracer::IntegralControl(){
+  int LIGHT_LOG_SIZE = 20;
+  int light_log[20];
+  int light_integra;
+  int diff;
+
+  if (line_status == true){
+    diff = colorSensor.getBrightness() - blue_target;
+  }else{
+    diff = colorSensor.getBrightness() - target;
+  }
+  light_log[light_log_index] = diff;
+	light_log_index = (light_log_index+1) % LIGHT_LOG_SIZE;
+  light_integra = 0;
+	for(int i=0;i<LIGHT_LOG_SIZE;i++){
+		light_integra += light_log[i];
+    //char s[256];
+    //sprintf(s, "%d", light_log[i]);
+    //syslog(7, s);
+	}
+  return (ki * (light_integra / LIGHT_LOG_SIZE));
+}
+
+//D制御
 float Tracer::derivative_control(){
-  int diff = colorSensor.getBrightness() - target;
+  int diff;
+  if (line_status == true){
+    diff = colorSensor.getBrightness() - blue_target;
+  }else{
+    diff = colorSensor.getBrightness() - target;
+  }
   return (kd * (diff - prev_diff));
   prev_diff = diff;
 }
@@ -50,32 +88,35 @@ float get_direction_change(int rm,int lm)
   return distance / 100;//仮 タイヤの間の距離mm
 }
 
-//I制御の簡単な実装。
-float Tracer::IntegralControl(){
-  int LIGHT_LOG_SIZE = 20;
-  int light_log[20];
-  int light_integra;
-
-  int diff = colorSensor.getBrightness() - target;
-  light_log[light_log_index] = diff;
-	light_log_index = (light_log_index+1) % LIGHT_LOG_SIZE;
-  light_integra = 0;
-	for(int i=0;i<LIGHT_LOG_SIZE;i++){
-		light_integra += light_log[i];
-    //char s[256];
-    //sprintf(s, "%d", light_log[i]);
-    //syslog(7, s);
-	}
-  return (ki * (light_integra / LIGHT_LOG_SIZE));
-}
-
 void Tracer::color_sensor(){
   colorSensor.getRawColor(rgb);
+}
+
+void Tracer::swing_neck(){
+  if (swing_time_start == true){
+    swing_time_start = false;
+    clock.reset();
+  }
+  syslog(7,"swing");
+  /*
+  char r[256];
+  sprintf(r,"%d",clock.now());
+  syslog(7,r);
+  */
+  if (clock.now() < 3000000){
+    syslog(7,"kita");
+    leftWheel.setPWM(0);
+    rightWheel.setPWM(0);
+  }else if(clock.now() >= 3000000){
+    leftWheel.setPWM(pwm);
+    rightWheel.setPWM(pwm);
+  }
 }
 
 void Tracer::run() {
   direction();
   color_sensor();
+
   /*
   char r[256];
   sprintf(r,"%d",rgb.r);
@@ -92,29 +133,72 @@ void Tracer::run() {
   */
   msg_f("running...", 1);
   double b_r_difference = (double)rgb.b / (rgb.r + 1);
-  double b_g_dirrerence = (double)rgb.b / (rgb.g + 1);
+  double b_g_difference = (double)rgb.b / (rgb.g + 1);
+  double r_b_difference = (double)rgb.r / (rgb.b + 1);
+  double g_b_difference = (double)rgb.g / (rgb.b + 1);
+  
   /*
   char r_dif[256];
   sprintf(r_dif,"%lf",b_r_difference);
   syslog(7,"r_dif");
   syslog(7,r_dif);
   char g_dif[256];
-  sprintf(g_dif,"%lf",b_g_dirrerence);
+  sprintf(g_dif,"%lf",b_g_difference);
   syslog(7,"g_dif");
   syslog(7,g_dif);
   */
-  if ((b_r_difference > 1.5) & (b_g_dirrerence > 1.5)){
-    syslog(7,"青色に入った");
-  }
+
+  /*
+  char r_dif[256];
+  sprintf(r_dif,"%lf",r_b_difference);
+  syslog(7,"r_dif");
+  syslog(7,r_dif);
+  char g_dif[256];
+  sprintf(g_dif,"%lf",g_b_difference);
+  syslog(7,"g_dif");
+  syslog(7,g_dif);
+  */
+  /*
   char brightness[256];
   sprintf(brightness,"%d",colorSensor.getBrightness());
   syslog(7,brightness);
-  float turn = calc_porp_value()+derivative_control() + IntegralControl();
-  int pwm_l = pwm - turn;
-  int pwm_r = pwm + turn;
-  leftWheel.setPWM(pwm_l);
-  rightWheel.setPWM(pwm_r);
+  */
+  if ((b_r_difference > 1.5) & (b_g_difference > 1.5)){
+    syslog(7,"青色に入った");
+    line_status = true;
+    float turn = calc_porp_value()+derivative_control()+IntegralControl();
+    int pwm_l = pwm - turn;
+    int pwm_r = pwm + turn;
+    leftWheel.setPWM(pwm_l);
+     rightWheel.setPWM(pwm_r);
+  } else if ((r_b_difference > 1.5) & (g_b_difference > 1.5)){
+    syslog(7,"黄色に入った");
+    if (yellow_in_flag == false){
+      syslog(7,"きた");
+      yellow_count += 1;
+    }
+    yellow_in_flag = true;
+    
+    float turn = calc_porp_value()+derivative_control() + IntegralControl();
+    int pwm_l = pwm + turn;
+    int pwm_r = pwm + turn;
+    leftWheel.setPWM(pwm_l);
+    rightWheel.setPWM(pwm_r);
+  }else{
+    yellow_in_flag = false;
+    if ((yellow_count == 2) & (swing_start == true)){
+      swing_neck();
+    }else{
+      line_status = false;
+      float turn = calc_porp_value() + derivative_control() + IntegralControl();
+      int pwm_l = pwm - turn;
+      int pwm_r = pwm + turn;
+      leftWheel.setPWM(pwm_l);
+      rightWheel.setPWM(pwm_r);
+    }
+  }
   
 }
+
 
 
--- app/Tracer.h
+++ app/Tracer.h
@@ -1,6 +1,7 @@
 #include "Motor.h"       
 #include "ColorSensor.h" 
-#include "util.h"        
+#include "util.h"  
+#include "Clock.h"
 
 using namespace ev3api;  
 
@@ -16,26 +17,28 @@ public:
 private:
   Motor leftWheel;
   Motor rightWheel;
-  ColorSensor colorSensor; 
+  ColorSensor colorSensor;
+  Clock clock; 
   const int8_t mThreshold = 28;  
   //最高速度
   const int8_t maxPwm = (Motor::PWM_MAX) / 1;
   //直線の最高速度
   const int8_t straightMaxPwm = (Motor::PWM_MAX) / 1;
-  const int8_t pwm = (Motor::PWM_MAX) / 5;
+  const int8_t pwm = (Motor::PWM_MAX) / 2;
   int tracerStatus = 0;
   float body_direction;
   float calc_porp_value();
   float IntegralControl();
   float derivative_control();
   void direction();
+  void swing_neck();
   int32_t left_counts;
   int32_t right_counts;
   const int target = 19;
   //青い線を通る時の基準値
   const int blue_target = 26;
   int prev_diff = 0;
-  const float Kp = 0.83;
+  const float kp = 0.83;
   //pwmの値がmax / 1　の時
   const float maxkp = 1.0;
   const float kd = 0;
@@ -43,4 +46,11 @@ private:
   int light_log_index = 0;
   void color_sensor();
   rgb_raw_t rgb;
+  bool line_status=false;
+  int yellow_count = 0;
+  int map_status=0;
+  bool swing = true;
+  bool yellow_in_flag=false;
+  bool swing_start = true;
+  bool swing_time_start = true;
 };
